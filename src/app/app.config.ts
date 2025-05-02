a2b67967ec54375a2b624278a38e5cfa166cfdd3
Rui Zhao <rzhao7815@gmail.com>
2025-05-02 14:36:49 -0500

    feat: add provideHttpClient() to enable HTTP support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,5 +5,4 @@ import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient()]
-
 };
